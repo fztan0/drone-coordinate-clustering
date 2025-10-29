70ea7a3706b80f76ff14ecd87a5cc07e7c4f1a0e
Jeff Tan <JeffTan.b@gmail.com>
2025-10-28 20:47:54 -0700

    add basic file input and data loading

---
+++ src/file_handler.py
@@ -0,0 +1,19 @@
+import numpy
+
+
+def get_file_name() -> str:
+    input_file_name = input("Enter the name of file: ")
+
+    if not input_file_name:
+        raise ValueError("No file name provided")
+
+    # input data files are placed in ./data of repo root
+    input_file_name = "data/" + input_file_name
+
+    return input_file_name
+
+def load_file_coordinates(file_path: str) -> numpy.ndarray:
+    # numpy.loadtxt() should already handle majority of error flags for us
+    file_coordinate_data = numpy.loadtxt(file_path, dtype=numpy.float64)
+
+    return file_coordinate_data
---
+++ src/main.py
@@ -0,0 +1,10 @@
+import file_handler
+
+def main():
+    input_data = file_handler.load_file_coordinates(file_handler.get_file_name())
+    for point in input_data:
+        print(point)
+
+
+if __name__ == "__main__":
+    main()
