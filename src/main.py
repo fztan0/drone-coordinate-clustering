b60d159654d50fa50a23f3ad73b0be8981bfbd02
Jeff Tan <JeffTan.b@gmail.com>
2025-11-03 19:56:50 -0800

    change module name invoked

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import file_handler
-import math_utils
+import math_utilities
 
 def main():
     input_data = file_handler.load_file_coordinates(file_handler.get_file_name())
 
-    distance_matrix = math_utils.generate_distance_matrix(input_data)
+    distance_matrix = math_utilities.generate_distance_matrix(input_data)
     print(distance_matrix)
 
 if __name__ == "__main__":
