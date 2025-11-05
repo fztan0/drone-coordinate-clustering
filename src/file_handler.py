35f64485cc039541b94d84544fb4a26006ff0d6e
randall-nguyen <rnguy137@ucr.edu>
2025-11-05 11:59:26 -0800

    updated and cleaned up the implementation of ui

--- src/file_handler.py
+++ src/file_handler.py
@@ -1,4 +1,5 @@
 import numpy
+import os
 
 def get_file_name() -> str:
     input_file_name = input("Enter the name of file: ")
@@ -18,9 +19,10 @@ def get_user_choice(output_file_name: str) -> List[str]:
     if not user_choice:
         raise ValueError("Invalid choice")
     
+    # list containing all the solutions; easy output
     output_files = []
     for x in range(1, choice + 1):
-        output_files.append(f"{output_file_name}_{x}_SOLUTION_{000}.txt")
+        output_files.append(f"{output_file_name}_{x}_SOLUTION_{sub_distance}.txt")
 
     return output_files
 
@@ -28,20 +30,27 @@ def get_user_choice(output_file_name: str) -> List[str]:
 def compute_possible_solutions() -> None:
     print(f"ComputePossibleSolutions")
 
-    input_file_path = get_file_name
-    input_file_name = os.path.splitext(input_file_path)[0]
+    input_file_name = get_file_name()
+    coordinates = load_file_coordinates(input_file_name)
+
+    # remove .txt extension before passing to other functions for append
+    input_file_name = os.path.splitext(input_file_name)[0]
     output_file_name = f"{input_file_name}"
 
-    coordinates = load_coordinate_data(input_file_name)
     n = len(coordinates)
     print(f"There are {n} nodes: Solutions will be available by 7:04am")
 
-    for x in range(1,5):
-        for i in range(1, x + 1):
-            print(f"    {i}. Landing Pad {i} should be at {location_coordinates}, serving {n} locations, route is {route_distance} meters")
+    # distance_matrix = compute_distance_matrix(coordinates, n)
 
+    for x in range(1,5):
+        print(f"{x}) If you use {x} drone(s), the total route will be {total_distance} meters")
+        for i in range(1, x + 1):
+            print(f"    {i}. Landing Pad {i} should be at {location_coordinates(x)}, serving {n} locations, route is {individual_route_distance(x)} meters")
+
+    # "please select your choice 1 to 4:"
     file_solutions = get_user_choice(output_file_name)
 
+    # might include line 51 to be apart of save_route_to_text_file 
     print(f"Writing {file_solutions} to disk.")
 
 def load_file_coordinates(file_path: str) -> numpy.ndarray:
